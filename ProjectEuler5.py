89fe579096d502832e3ee14c82b7afe94f9ecedc
ashwin nalwade <tcs15b007@iittp.ac.in>
2018-03-15 18:35:27 +0530

    Create ProjectEuler5.py

---
+++ ProjectEuler5.py
@@ -0,0 +1,8 @@
+#!/bin/python3
+
+from math import gcd
+from functools import reduce
+
+for _ in range(int(input())):
+    N = int(input())
+    print(reduce(lambda x,y: x*y//gcd(x,y), range(1,N+1)))
